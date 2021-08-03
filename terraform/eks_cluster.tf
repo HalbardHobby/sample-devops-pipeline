48c32efafdfc59c9fb85db16a235e46950844ceb
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-03 14:16:25 -0500

    ⛰ Added autoscaling group

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -6,3 +6,22 @@ resource "aws_eks_cluster" "cluster" {
     subnet_ids = [for subnet in aws_subnet.private : subnet.id]
   }
 }
+
+resource "aws_autoscaling_group" "workers" {
+  name = "eks_cluster_autoscaling"
+  max_size = 5
+  min_size = 2
+  health_check_type = "EC2"
+  desired_capacity = 3
+
+  launch_template {
+    id = aws_launch_template.worker_template.id
+    version = "$latest"
+  }
+}
+
+resource "aws_launch_template" "worker_template" {
+  name_prefix = "worker"
+  image_id = "ami-0277b52859bac6f4b"
+  instance_type = "t2.micro"
+}
