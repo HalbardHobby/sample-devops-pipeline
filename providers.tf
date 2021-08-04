5f5f79ad09a3ab3fe72a5d84e88f812b14c70173
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-04 17:42:11 -0500

    ☸ Configured kubernetes provider

--- providers.tf
+++ providers.tf
@@ -16,3 +16,9 @@ provider "aws" {
   profile = "default"
   region  = "us-east-2"
 }
+
+provider "kubernetes" {
+  host = data.aws_eks_cluster.cluster.endpoint
+  token = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
